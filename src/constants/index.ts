6db1a0d1ada321c6db124b461cda2848dbbdeedf
seul-dev <seul.dev@gmail.com>
2023-01-02 22:51:31 +0900

    ✨ feat: [#3] Header component 구현

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,11 @@
-export {};
+export const PAGEURL = {
+  ROOT: '/',
+  NOTFOUND: '*',
+  SIGNIN: '/auth/signin',
+  SIGNUP: '/auth/signup',
+  MYSTAT: '/mystat',
+  MYPAGE: '/mypage',
+};
+
+// TODO
+// 새로 추가되는 페이지 컴포넌트명 - url 정의하기
