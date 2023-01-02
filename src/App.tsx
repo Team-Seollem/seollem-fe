166c49cb496aea1f0916af4153f3a9528b6a486c
seul-dev <seul.dev@gmail.com>
2023-01-03 00:56:18 +0900

    Revert "✨feat: [#3] Layout component 구현"
    This reverts commit e12cf1e280b759096b29d628f53d9b455d3d0fde.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-import Layout from '@components/layout/Layout';
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyle, theme } from './styles';
-
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Layout />
-    </ThemeProvider>
-  );
+  return <div>hello!</div>;
 }
 
 export default App;
