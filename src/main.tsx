d126ed22ffc2bb283526c2179de968028ff25d30
seul-dev <seul.dev@gmail.com>
2023-01-03 01:17:34 +0900

    Revert "✨ feat: 레이아웃 관련 컴포넌트 이관 및 ui 수정(Header, Layout, PageTitle)" (#11)
    * Revert "✨ feat: [#3] Page Title Component 구현"
    
    This reverts commit 17afacaa3e40aa190c2bc5f7d89a081e563612a5.
    
    * Revert "🔧 config: [#3] eslint rule 추가(default props 추가에 관한 규칙 off)"
    
    This reverts commit 3937869ae3ddd253be1ecaf1ae2c5797d5236f81.
    
    * Revert "✨feat: [#3] Layout component 구현"
    
    This reverts commit e12cf1e280b759096b29d628f53d9b455d3d0fde.
    
    * Revert "✨ feat: [#3] Header component 구현"
    
    This reverts commit f22770ff789e3e5beb5de2de3d7832aa356cec68.
    
    * Revert "🔧 config: [#3] eslint 규칙 추가"
    
    This reverts commit ede7a05f6b53c9832cb293ab4c83b916884ad538.
    
    * Revert ":wrenhc: config: .prettierignore 추가"
    
    This reverts commit c3c51c2af5cdc2d1e983653fc83c95a6449ea721.
    
    * Revert ":wrench: config: tsconfig 설정 오타 수정"
    
    This reverts commit a4b18263a9836dce13caec259ce4e434ba26d0e0.

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle, theme } from './styles';
 import App from './App';
-import { PAGEURL } from './constants';
-
-const router = createBrowserRouter([
-  {
-    path: PAGEURL.ROOT,
-    element: <App />,
-    children: [
-      { index: true, element: <div>main page</div> },
-      { path: PAGEURL.NOTFOUND, element: <div>not found</div> },
-    ],
-  },
-]);
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
