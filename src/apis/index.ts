bde1b8f7c16c0e17962e9d3bf31b7523cd037f7a
StarryPro <hansunguk6178@gmail.com>
2023-01-03 11:49:13 +0900

    :recycle: refactor: [#6] 책 검색(외부API) 비동기 통신 코드 분리

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,1 +1,7 @@
-export {};
+import axios from 'axios';
+
+// 책 검색 (외부API)
+export const getBookInfo = async (keyword: string) => {
+  const { data } = await axios.get(`https://seollem.link/ext-lib/${keyword}`);
+  return data;
+};
