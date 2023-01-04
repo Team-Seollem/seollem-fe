7af149edc51f8099d269fc0c97eea0e878e89250
StarryPro <hansunguk6178@gmail.com>
2023-01-04 23:35:05 +0900

    :sparkles: feat: [#14] BookInfo 타입 추가

--- src/types/basic.ts
+++ src/types/basic.ts
@@ -6,9 +6,15 @@ export interface Book {
   publisher: string;
 }
 
-export interface SearchBookInfo extends Book {
-  data: SearchBookInfo[];
+export interface SearchBookInfo {
   itemPage: number;
+  title: string;
+  author: string;
+  cover: string;
+  publisher: string;
+}
+export interface BookInfoProps {
+  bookInfoData: SearchBookInfo;
 }
 export interface SearchProps {
   onChangeInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
