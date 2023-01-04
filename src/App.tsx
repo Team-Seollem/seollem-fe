e333dc0291c299e989078a49b56c7af1b37a772f
StarryPro <hansunguk6178@gmail.com>
2023-01-04 23:38:34 +0900

    🌱 chore: [#3] 기존 레이아웃

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import Layout from '@components/layout/Layout';
+import { ThemeProvider } from 'styled-components';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { GlobalStyle, theme } from './styles';
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <div>hello!</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Layout />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
