b0cb7f4ad586348e35b557c641beb3497a67fb46
seul-dev <seul.dev@gmail.com>
2023-01-06 01:10:19 +0900

    ✨ feat: [#8] BookCoverItem component 구현

---
+++ src/components/common/BookCoverItem.tsx
@@ -0,0 +1,34 @@
+import styled, { css } from 'styled-components';
+
+type Props = {
+  src: string;
+  onClick?: (event: React.MouseEvent<HTMLImageElement>) => void;
+  width?: string;
+};
+
+export default function BookCoverItem({ src, onClick, width }: Props) {
+  return (
+    <Wrapper>
+      <Img src={src} alt="book_cover" onClick={onClick} width={width} />
+    </Wrapper>
+  );
+}
+const ACTIVE = css`
+  cursor: pointer;
+  transition: transfrom 300ms ease-in;
+  &:hover {
+    transform: scale(1.02);
+  }
+`;
+
+const Wrapper = styled.div`
+  display: inline-block;
+  padding: 0.3rem;
+`;
+
+const Img = styled.img<Props>`
+  border-radius: 0.3rem;
+  box-shadow: 0 4px 6px rgb(32 33 36 / 28%);
+  ${(props) => props.onClick && ACTIVE}
+  width: ${(props) => props.width || '5.3rem'};
+`;
