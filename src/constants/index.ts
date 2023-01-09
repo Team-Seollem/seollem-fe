752093f0f8e2926383a51d81daeee849d4428b0e
seul-dev <seul.dev@gmail.com>
2023-01-09 14:09:03 +0900

    🎨 style: 상수 변수명 수정

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,8 @@
-export const PAGEURL = {
+export const PAGE_URL = {
   ROOT: '/',
-  NOTFOUND: '*',
-  SIGNIN: '/auth/signin',
-  SIGNUP: '/auth/signup',
+  NOT_FOUND: '*',
+  SIGN_IN: '/auth/signin',
+  SIGN_UP: '/auth/signup',
   MYSTAT: '/mystat',
   MYPAGE: '/mypage',
   SEARCHBOOK: '/search/book',
