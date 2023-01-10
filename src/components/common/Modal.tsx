996aef54e017104d69e19d944a4a19379bf4e238
seul-dev <seul.dev@gmail.com>
2023-01-10 16:30:18 +0900

    ✨ feat: [#9] Modal component 구현(react portal)

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
 import styled from 'styled-components';
 import Button from './Button';
+import ReactPortal from './ReactPortal';
 
-interface ModalProps {
+interface Props {
   closeModal: () => void;
-  children?: React.ReactNode;
+  children: React.ReactNode;
+}
+export default function Modal({ closeModal, children }: Props) {
+  return (
+    <ReactPortal wrapperId="modal-root">
+      <ModalBackground onClick={closeModal}>
+        <ModalContainer
+          onClick={(event: React.MouseEvent) => event.stopPropagation()}
+        >
+          <Button
+            type="button"
+            styleType="ghost"
+            size="small"
+            onClick={closeModal}
+          >
+            X
+          </Button>
+          {children}
+        </ModalContainer>
+      </ModalBackground>
+    </ReactPortal>
+  );
 }
 
 const ModalBackground = styled.div`
@@ -31,20 +52,15 @@ const ModalContainer = styled.div`
   background-color: ${({ theme }) => theme.color.white};
   color: ${({ theme }) => theme.color.gray01};
 `;
-
-function Modal({ closeModal, children }: ModalProps) {
-  return (
-    <ModalBackground onClick={closeModal}>
-      <ModalContainer
-        onClick={(event: React.MouseEvent) => event.stopPropagation()}
-      >
-        <Button type="button" styleType="ghost" size="small">
-          X
-        </Button>
-        {children}
-      </ModalContainer>
-    </ModalBackground>
-  );
-}
-
-export default Modal;
+/**
+ * 배경을 클릭하면 모달이 닫힌다
+ * x 버튼을 클릭하면 모달이 닫힌다 -> 위치는 고정되어도 될 것 같음
+ * width, height 값을 props로 전달받을 수 있도록 한다.
+ *
+ * 모달을 보이게 하고(isOpen) closeModal => useModal 훅으로 만든다.
+ *
+ * (전역 상태로 만들 필요는 없을 듯 )
+ * potal을 사용할 것으로 예상되는 부분이 있다면 createPortal을 따로 컴포넌트로 만든다.
+ *
+ *
+ */
