f61c71ddcddab50f1b16a651777e40d13a582e75
StarryPro <hansunguk6178@gmail.com>
2023-01-12 14:57:51 +0900

    :sparkles: feat: [#19] 로그인 비동기 통신 코드 추가

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,7 +1,19 @@
+import { UserInfo } from '@projects/types/basic';
 import axios from 'axios';
 
 // 책 검색 (외부API)
 export const getBookInfo = async (keyword: string) => {
   const { data } = await axios.get(`https://seollem.link/ext-lib/${keyword}`);
   return data;
+};
+
+export const postSignIn = async (userInfoData: Omit<UserInfo, 'name'>) => {
+  const response = await axios.post(
+    `https://seollem.link/login`,
+    userInfoData,
+    {
+      withCredentials: true,
+    }
+  );
+  return response.headers.authorization;
 };
