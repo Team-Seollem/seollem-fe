3897a291d37e95c84f0ff22ee2511a2f353de056
StarryPro <hansunguk6178@gmail.com>
2023-01-16 01:01:03 +0900

    :sparkles: feat: [#20] SignUp page 구현

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,4 +2,5 @@ export { default as DetailBookInfoPage } from './DetailBookInfoPage';
 export { default as SearchBookPage } from './SearchBookPage';
 export { default as PostBookPage } from './PostBookPage';
 export { default as SignInPage } from './SignInPage';
+export { default as SignUpPage } from './SignUpPage';
 export { default as Landing } from './Landing';
