17754bdda2318e7cb5753785f7576922da54ac7e
StarryPro <hansunguk6178@gmail.com>
2023-01-16 01:07:04 +0900

    :sparkles: feat: [#20] SignInput component 수정

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,2 +1,4 @@
 export { default as Button } from './Button';
 export { default as PageTitle } from './PageTitle';
+export { default as SignInput } from './SignInput';
+export { default as SignContainer } from './SignContainer';
