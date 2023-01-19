aa11a3925b68a289aa0416bb1877fb84f2c69244
StarryPro <hansunguk6178@gmail.com>
2023-01-19 17:04:45 +0900

    :sparkles: feat: [#29] RecommendedSort component 구현

---
+++ src/components/RecommendedBooks/RecommendedSort.tsx
@@ -0,0 +1,45 @@
+import { RECOMMENDED_SORT } from '@constants';
+import styled from 'styled-components';
+
+type Props = {
+  onChangeList: (value: string) => void;
+};
+
+function RecommendedSort({ onChangeList }: Props) {
+  const sortDataList = RECOMMENDED_SORT.map((data) => {
+    return (
+      <li key={data.id}>
+        <button type="button" onClick={() => onChangeList(data.value)}>
+          {data.name}
+        </button>
+      </li>
+    );
+  });
+  return <SSortList>{sortDataList}</SSortList>;
+}
+
+export default RecommendedSort;
+
+const SSortList = styled.ul`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-bottom: 1rem;
+  border-bottom: solid 3px ${({ theme }) => theme.color.gray04};
+
+  li:hover {
+    border-bottom: solid 1px ${({ theme }) => theme.color.gray01};
+  }
+
+  button {
+    background-color: transparent;
+    font-family: 'Pretendard-Regular';
+    cursor: pointer;
+    padding: 1rem;
+    color: ${({ theme }) => theme.color.gray02};
+    border: none;
+    &:hover {
+      color: ${({ theme }) => theme.color.gray01};
+    }
+  }
+`;
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -10,9 +10,23 @@ export const PAGE_URL = {
   SEARCHBOOK: '/search/book',
   POSTBOOK: '/post/book/:param',
   DETAILBOOKINFO: '/detail/book/:param',
+  RECOMMENDEDBOOKS: '/recommended/books',
 };
 export const EMAIL_REGEX =
   /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/;
 
 export const PASSWORD_REGEX =
   /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{6,}$/;
+
+export const RECOMMENDED_SORT = [
+  {
+    id: 0,
+    name: '베스트셀러',
+    value: 'best-seller',
+  },
+  {
+    id: 1,
+    name: '주목할만한 신간 리스트',
+    value: 'item-new-special',
+  },
+];
