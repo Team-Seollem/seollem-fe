bb401fb4bbea0415defffa7f808c813a51432fd3
StarryPro <hansunguk6178@gmail.com>
2023-01-21 21:28:24 +0900

    :recycle: refactor:[#6] debounce 기법 적용 및 keyword 공백 제거

--- src/pages/SearchBookPage.tsx
+++ src/pages/SearchBookPage.tsx
@@ -1,15 +1,21 @@
-import Result from '@components/SearchBook/Result';
-import Search from '@components/SearchBook/Search';
+import SearchResult from '@components/SearchBook/SearchResult';
+import SearchKeyword from '@components/SearchBook/SearchKeyword';
 import { useState } from 'react';
+import useDebounce from '@hooks/useDebounce';
 
 function SearchBookPage() {
   const [keyword, setKeyword] = useState('');
-  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setKeyword(e.target.value);
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim();
+    setKeyword(value);
+  };
+
+  const searchQuery = useDebounce(keyword, 200);
+
   return (
     <>
-      <Search onChangeInput={onChangeInput} keyword={keyword} />
-      <Result keyword={keyword} />
+      <SearchKeyword onChangeInput={onChangeInput} keyword={keyword} />
+      <SearchResult searchQuery={searchQuery} />
     </>
   );
 }
