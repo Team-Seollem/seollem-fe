cd1b0fa230cd1c0442c784fef9409788eed3ad92
StarryPro <hansunguk6178@gmail.com>
2023-01-21 21:29:59 +0900

    :sparkles: feat: [#6] debounce custom hook 추가

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+const useDebounce = (value: string, milliSeconds: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, milliSeconds);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, milliSeconds]);
+
+  return debouncedValue;
+};
+
+export default useDebounce;
