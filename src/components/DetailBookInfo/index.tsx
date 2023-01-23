ea8f926b9a2847b6b1bae9e01da3a5b99ededf85
StarryPro <hansunguk6178@gmail.com>
2023-01-23 21:47:22 +0900

    :recycle: refactor: [#14] DetailBookInfo component 코드 개선 (공통 UI 컴포넌트 적용)

--- src/components/DetailBookInfo/index.tsx
+++ src/components/DetailBookInfo/index.tsx
@@ -1,6 +1,12 @@
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { SearchBookInfo } from '@projects/types/basic';
+import {
+  BookCoverItem,
+  BookDetailInfoItem,
+  Boxcontainer,
+  Button,
+} from '@components/common';
 
 type BookInfoProps = {
   bookInfoData: SearchBookInfo;
@@ -9,64 +15,28 @@ type BookInfoProps = {
 function DetailBookInfo({ bookInfoData }: BookInfoProps) {
   const navigate = useNavigate();
   const { cover, title, author, publisher, itemPage } = bookInfoData;
-  const handleClick = () =>
-    navigate(`/post/book/${title}`, {
+  const onHandleClick = () =>
+    navigate(`/book/register/${title}`, {
       state: {
         bookInfoData,
       },
     });
   return (
-    <SBookContainer>
-      <form>
-        <img src={cover} alt="도서 이미지" />
-        <SFormWrapper>
-          <label htmlFor="title">책 제목</label>
-          <input id="title" type="text" value={title} readOnly />
-          <label htmlFor="author">저자</label>
-          <input id="author" type="text" value={author} readOnly />
-          <label htmlFor="publisher">출판사</label>
-          <input id="publisher" type="text" value={publisher} readOnly />
-          <label htmlFor="itemPage">전체 페이지</label>
-          <input id="itemPage" type="number" value={itemPage || ''} readOnly />
-
-          <button type="button" onClick={handleClick}>
-            등록하러가기
-          </button>
-        </SFormWrapper>
-      </form>
-    </SBookContainer>
+    <Boxcontainer title={title}>
+      <BookCoverItem src={cover} />
+      <BookDetailInfoItem>
+        <label htmlFor="author">저자</label>
+        <input id="author" type="text" value={author} readOnly />
+        <label htmlFor="publisher">출판사</label>
+        <input id="publisher" type="text" value={publisher} readOnly />
+        <label htmlFor="itemPage">전체 페이지</label>
+        <input id="itemPage" type="number" value={itemPage || ''} readOnly />
+        <Button onClick={onHandleClick} size="small" styleType="solidPositive">
+          등록하러가기
+        </Button>
+      </BookDetailInfoItem>
+    </Boxcontainer>
   );
 }
 
 export default DetailBookInfo;
-
-export const SBookContainer = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 1rem 1.5rem;
-  border-radius: 0.25rem;
-  box-shadow: 0rem 0rem 0.25rem 0rem rgba(0 0 0 / 20%);
-`;
-export const SFormWrapper = styled.div`
-  width: 100%;
-  label {
-    display: block;
-    margin-bottom: 0.5rem;
-    font-weight: 300;
-    font-size: 1.2rem;
-    color: ${(props) => props.theme.color.gray01};
-  }
-  input,
-  select {
-    margin-bottom: 0.5rem;
-    padding: 0.5rem 0.75rem;
-    border: 0.1rem solid ${(props) => props.theme.color.skyblue02};
-    border-radius: 0.25rem;
-    outline: none;
-    color: ${(props) => props.theme.color.gray01};
-    font-family: 'Pretendard-Regular';
-    width: 100%;
-  }
-`;
