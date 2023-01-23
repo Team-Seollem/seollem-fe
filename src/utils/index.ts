9aa7948f9f775fd7423a0a7299e4c055ea17b139
StarryPro <hansunguk6178@gmail.com>
2023-01-23 21:36:09 +0900

    :bug: fix: HandleClickBookInfo 유틸 함수 삭제

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,1 @@
-import { SearchBookInfo } from '@projects/types/basic';
-import { useNavigate } from 'react-router-dom';
-
-// 책 추천 및 검색 페이지에서 책 정보 클릭 시 호출되는 함수
-export const HandleClickBookInfo = (bookInfoData: SearchBookInfo) => {
-  const navigate = useNavigate();
-  navigate(`/detail/book/${bookInfoData.title}`, {
-    state: {
-      bookInfoData,
-    },
-  });
-};
+export {};
