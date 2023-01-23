bf1c5bcb3ab64f964e14320034e982a977044860
StarryPro <hansunguk6178@gmail.com>
2023-01-23 22:38:31 +0900

    :recycle: refactor: PageTitle 공통 UI 컴포넌트 적용

--- src/components/DetailBookInfo/index.tsx
+++ src/components/DetailBookInfo/index.tsx
@@ -6,6 +6,7 @@ import {
   BookDetailInfoItem,
   Boxcontainer,
   Button,
+  PageTitle,
 } from '@components/common';
 
 type BookInfoProps = {
@@ -22,20 +23,27 @@ function DetailBookInfo({ bookInfoData }: BookInfoProps) {
       },
     });
   return (
-    <Boxcontainer title={title}>
-      <BookCoverItem src={cover} />
-      <BookDetailInfoItem>
-        <label htmlFor="author">저자</label>
-        <input id="author" type="text" value={author} readOnly />
-        <label htmlFor="publisher">출판사</label>
-        <input id="publisher" type="text" value={publisher} readOnly />
-        <label htmlFor="itemPage">전체 페이지</label>
-        <input id="itemPage" type="number" value={itemPage || ''} readOnly />
-        <Button onClick={onHandleClick} size="small" styleType="solidPositive">
-          등록하러가기
-        </Button>
-      </BookDetailInfoItem>
-    </Boxcontainer>
+    <>
+      <PageTitle title="책 정보 상세" />
+      <Boxcontainer title={title}>
+        <BookCoverItem src={cover} />
+        <BookDetailInfoItem>
+          <label htmlFor="author">저자</label>
+          <input id="author" type="text" value={author} readOnly />
+          <label htmlFor="publisher">출판사</label>
+          <input id="publisher" type="text" value={publisher} readOnly />
+          <label htmlFor="itemPage">전체 페이지</label>
+          <input id="itemPage" type="number" value={itemPage || ''} readOnly />
+          <Button
+            onClick={onHandleClick}
+            size="small"
+            styleType="solidPositive"
+          >
+            등록하러가기
+          </Button>
+        </BookDetailInfoItem>
+      </Boxcontainer>
+    </>
   );
 }
 
