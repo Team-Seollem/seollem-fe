aaec997ae104b8bd798b9ebcd28ebbf7d1372ba9
seul-dev <seul.dev@gmail.com>
2023-01-28 23:46:30 +0900

    etc [#30] 환경변수 설정

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,15 +1,17 @@
 import { UserInfo } from '@projects/types/basic';
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
+
 // 책 검색 (외부API)
 export const getBookInfo = async (keyword: string) => {
-  const { data } = await axios.get(`https://seollem.link/ext-lib/${keyword}`);
+  const { data } = await axios.get(`${BASE_URL}ext-lib/${keyword}`);
   return data;
 };
 
 // 베스트셀러 조회 | 주목할만한 신간 리스트 조회 외부(API)
 export const getRecommendedBooksList = async (sort: string) => {
-  const { data } = await axios.get(`https://seollem.link/ext-lib/${sort}`);
+  const { data } = await axios.get(`${BASE_URL}ext-lib/${sort}`);
   return data;
 };
 
@@ -17,13 +19,9 @@ export const getRecommendedBooksList = async (sort: string) => {
 export const postSignIn = async (
   SignInuserInfoData: Pick<UserInfo, 'email' | 'password'>
 ) => {
-  const response = await axios.post(
-    `https://seollem.link/login`,
-    SignInuserInfoData,
-    {
-      withCredentials: true,
-    }
-  );
+  const response = await axios.post(`${BASE_URL}login`, SignInuserInfoData, {
+    withCredentials: true,
+  });
   return response.headers.authorization;
 };
 
@@ -31,12 +29,8 @@ export const postSignIn = async (
 export const postSignUp = async (
   SignUpuserInfoData: Omit<UserInfo, 'password_confirm'>
 ) => {
-  const response = await axios.post(
-    `https://seollem.link/join`,
-    SignUpuserInfoData,
-    {
-      withCredentials: true,
-    }
-  );
+  const response = await axios.post(`${BASE_URL}join`, SignUpuserInfoData, {
+    withCredentials: true,
+  });
   return response;
 };
