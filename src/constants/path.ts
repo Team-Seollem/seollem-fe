6c3983e19637bc4f4e134a2be8a4bfe928f4bd9b
seul-dev <seul.dev@gmail.com>
2023-01-28 23:38:25 +0900

    🎨 style: [#30] constants 성격에 맞게 파일 분리

---
+++ src/constants/path.ts
@@ -0,0 +1,12 @@
+export const PAGE_URL = {
+  ROOT: '/',
+  NOT_FOUND: '*',
+  SIGN_IN: '/auth/signin',
+  SIGN_UP: '/auth/signup',
+  MYSTAT: '/mystat',
+  MYPAGE: '/mypage',
+  SEARCHBOOK: '/book/search',
+  POSTBOOK: '/book/register/:id',
+  DETAILBOOKINFO: '/book/detail/:id',
+  RECOMMENDEDBOOKS: '/book/recommended',
+};
