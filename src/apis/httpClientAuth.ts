0ba74bc9086afc4849f10d366314d8d39d70e516
seul-dev <seul.dev@gmail.com>
2023-01-31 01:33:30 +0900

    etc: [#30] 작동하지 않는 코드 삭제(errorHanldler 내부의 useNavigate 훅)

--- src/apis/httpClientAuth.ts
+++ src/apis/httpClientAuth.ts
@@ -1,8 +1,6 @@
 import type { AxiosRequestConfig, AxiosError } from 'axios';
-import { useNavigate } from 'react-router-dom';
 import { HttpClientImpl, HttpClient } from './httpClient';
 import type { TokenRepositoryImpl } from './tokenRepository';
-import { PAGE_URL } from '../constants/path';
 
 const addAuthHeader = (
   config: AxiosRequestConfig,
@@ -20,9 +18,6 @@ const handleUnauthorizedError = (
 ) => {
   if (error.response?.status === 403) {
     tokenRepository.removeToken();
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const navigate = useNavigate();
-    navigate(PAGE_URL.SIGN_IN);
   }
   throw error;
 };
