fa48e7c8236087107e92987daca6782162dd3cdb
seul-dev <seul.dev@gmail.com>
2023-01-30 16:16:30 +0900

    ♻️  refactor: [#30] util 함수로 분리

---
+++ src/utils/isValidToken.ts
@@ -0,0 +1,15 @@
+import jwtDecode from 'jwt-decode';
+
+type JwtPayload = {
+  sub: string;
+  id: number;
+  exp: number;
+  email: string;
+};
+
+export const isValidToken = (token: string) => {
+  const { exp: expireTime } = jwtDecode<JwtPayload>(token);
+  const currentTime = Math.floor(Date.now() / 1000);
+
+  return currentTime < expireTime;
+};
