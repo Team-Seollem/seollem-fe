80976f5ad94a3054210d111cb75697098b9d70e9
seul-dev <seul.dev@gmail.com>
2023-01-31 04:24:46 +0900

    ✨ feat: [#30] queryClient -queryErrorHandler 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,23 @@ import { Layout } from '@components/layout';
 import { ThemeProvider } from 'styled-components';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { GlobalStyle, theme } from '@styles';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const queryClient = new QueryClient();
+import { GlobalStyle, theme } from '@styles';
+import { queryErrorHandler } from 'utils/queryErrorHandler';
+
+const defaultQueryClientOptions = {
+  queries: {
+    onError: queryErrorHandler,
+  },
+  mutations: {
+    onError: queryErrorHandler,
+  },
+};
+const queryClient = new QueryClient({
+  defaultOptions: defaultQueryClientOptions,
+});
 
 function App() {
   return (
@@ -14,6 +28,7 @@ function App() {
         <Layout />
       </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
+      <ToastContainer position="top-center" pauseOnFocusLoss theme="light" />
     </QueryClientProvider>
   );
 }
