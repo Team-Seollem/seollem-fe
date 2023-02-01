fbbc5b1dde2abc95fff626feab1b53bb6d33f4ac
seul-dev <seul.dev@gmail.com>
2023-02-01 15:47:39 +0900

    etc: [#42] 사용하지 않는 import 삭제

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,8 +1,8 @@
 import { Navigate, Outlet } from 'react-router-dom';
-import useLoginState from '@hooks/useLoginState';
-import { PAGE_URL } from '@constants';
 import { useRecoilState } from 'recoil';
-import { loginState } from '../state/atom';
+
+import { PAGE_URL } from '@constants';
+import { loginState } from '@state/atom';
 
 function PrivateRoute() {
   const [isLoggedIn] = useRecoilState(loginState);
