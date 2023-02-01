14e4ea2f776f7af64c639609fd3eeab23a573828
seul-dev <seul.dev@gmail.com>
2023-02-01 15:27:11 +0900

    ✨ feat: [#42] useLoginState 구현

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 import { Outlet } from 'react-router-dom';
+import useLoginState from '@hooks/useLoginState';
 import Header from './Header';
 
 export default function Layout() {
+  const { isLoggedIn } = useLoginState();
   return (
     <>
       <Header />
