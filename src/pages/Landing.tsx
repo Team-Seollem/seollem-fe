0180613d608065e94fea53ff89ed345481d164dc
seul-dev <seul.dev@gmail.com>
2023-02-04 01:34:44 +0900

    ✨ [#44] gnb component ui 구현

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -2,13 +2,8 @@ import { Button } from '@components/common';
 
 export default function Landing() {
   return (
-    <>
-      <Button type="button" styleType="solidPositive" size="medium">
-        서비스 체험하기
-      </Button>
-      <Button type="button" styleType="neutral" size="medium">
-        로그인
-      </Button>
-    </>
+    <Button type="button" styleType="solidPositive" size="medium">
+      서비스 체험하기
+    </Button>
   );
 }
