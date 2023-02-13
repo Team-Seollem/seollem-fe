258d99a6f145c672f87a7ebf556f3fe8a297e46d
StarryPro <hansunguk6178@gmail.com>
2023-02-13 11:12:47 +0900

    :lipstick: design: [#38] SignContainter components ui 수정

--- src/components/common/SignContainer.tsx
+++ src/components/common/SignContainer.tsx
@@ -3,24 +3,18 @@ import styled from 'styled-components';
 
 type Props = {
   children: React.ReactNode;
-  width: string;
   height: string;
   onSubmit: React.FormEventHandler<HTMLFormElement>;
 };
-export default function SignContainer({
-  children,
-  width,
-  height,
-  onSubmit,
-}: Props) {
+export default function SignContainer({ children, height, onSubmit }: Props) {
   return (
-    <SContainer width={width} height={height}>
+    <SContainer height={height}>
       <form onSubmit={onSubmit}>{children}</form>
     </SContainer>
   );
 }
 
-const SContainer = styled.section<Pick<Props, 'width' | 'height'>>`
+const SContainer = styled.section<Pick<Props, 'height'>>`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -28,7 +22,8 @@ const SContainer = styled.section<Pick<Props, 'width' | 'height'>>`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: ${(props) => props.width};
+  margin-top: 2rem;
+  width: 22rem;
   height: ${(props) => props.height};
   border-radius: 0.25rem;
   box-shadow: 0rem 0rem 0.25rem 0rem rgba(0 0 0 / 20%);
@@ -40,6 +35,6 @@ const SContainer = styled.section<Pick<Props, 'width' | 'height'>>`
     display: flex;
     flex-direction: column;
     justify-content: space-around;
-    align-items: flex-start;
+    align-items: flex-end;
   }
 `;
