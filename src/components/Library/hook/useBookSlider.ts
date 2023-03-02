b5a93e3d3d35c0fd99eb848ae4609f56a6f669f7
seul-dev <seul.dev@gmail.com>
2023-03-02 21:06:19 +0900

    ♻️  refactor: [#22] useBookSlider hook으로 분리

---
+++ src/components/Library/hook/useBookSlider.ts
@@ -0,0 +1,24 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import type { BookStatus } from '@projects/types/library';
+import { bookService } from '@apis/index';
+import { CACHE_KEYS } from 'constants/cacheKey';
+
+type UseBookSliderProps = {
+  bookStatus: BookStatus;
+};
+
+export const useBookSlider = ({ bookStatus }: UseBookSliderProps) => {
+  return useInfiniteQuery(
+    CACHE_KEYS.library(bookStatus),
+    ({ pageParam = 1 }) => bookService.getLibrary(pageParam, bookStatus),
+
+    {
+      getNextPageParam: (lastpage) => {
+        const { pageInfo } = lastpage;
+        const nextPage = pageInfo.page + 1;
+
+        return nextPage <= pageInfo.totalPages ? nextPage : undefined;
+      },
+    }
+  );
+};
