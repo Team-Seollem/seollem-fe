4fbec5c7fa0215d4cd4b4dfebccd4c2d3d7e3a12
seul-dev <seul.dev@gmail.com>
2023-03-08 17:30:57 +0900

    💄 design: [#22] slider style 추가

--- src/components/Library/BookSlider.tsx
+++ src/components/Library/BookSlider.tsx
@@ -20,11 +20,11 @@ export default function BookSlider({ bookStatus: status }: Props) {
 
   return (
     <>
-      <h1>{bookStatus[status]}</h1>
+      <Title>{bookStatus[status]}</Title>
       <Container>
         <Swiper
-          spaceBetween={50}
-          slidesPerView={3}
+          spaceBetween={5}
+          slidesPerView={4}
           onSlideChange={() => console.log('slide change')}
           onSwiper={(swiper) => console.log(swiper)}
         >
@@ -39,12 +39,34 @@ export default function BookSlider({ bookStatus: status }: Props) {
             ))
           )}
         </Swiper>
+        <BookShelf />
       </Container>
     </>
   );
 }
 
+const Title = styled.h1`
+  color: ${({ theme }) => theme.color.black};
+  font-size: ${({ theme }) => theme.fontSize.base};
+  font-weight: bold;
+  margin-bottom: 0.3rem;
+`;
+
 const Container = styled.div`
   width: 100%;
-  margin-bottom: 2rem;
+
+  margin-bottom: 1rem;
+  align-items: baseline;
+  .swiper-wrapper {
+    align-items: baseline;
+    z-index: 0;
+    padding-left: 2rem;
+    padding-right: 2rem;
+  }
+`;
+
+const BookShelf = styled.div`
+  width: 100%;
+  height: 0.7rem;
+  box-shadow: 0px -1px 6px rgba(0, 0, 0, 0.05), 0px 4px 16px rgba(0, 0, 0, 0.25);
 `;
