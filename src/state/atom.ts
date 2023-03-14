712ccc3a15f9fa169b73ae458aaf08d713c2f11e
seul-dev <seul.dev@gmail.com>
2023-03-14 15:35:59 +0900

    🐛 fix: [#56] atom 초기값 수정

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,6 +1,8 @@
 import { atom, type RecoilState } from 'recoil';
+import { tokenRepository } from '@apis/index';
+import { isValidToken } from '@utils';
 
 export const loginState: RecoilState<boolean> = atom({
   key: 'isLoggedIn',
-  default: false,
+  default: isValidToken(tokenRepository.getToken()),
 });
