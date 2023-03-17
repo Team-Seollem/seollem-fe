d02626d2ece0303364e3768785e2b679cd569a17
seul-dev <seul.dev@gmail.com>
2023-03-17 20:12:51 +0900

    ✨ feat: [#56] MemoTypeSelect 컴포넌트

--- src/pages/MemoForm.tsx
+++ src/pages/MemoForm.tsx
@@ -1,10 +1,32 @@
-import { PageTitle } from '@components/common';
+import { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import { Boxcontainer, PageTitle } from '@components/common';
+import { MemoType } from '@projects/types/library';
+import MemoTypeSelect from '@components/MemoForm/MemoTypeSelect';
 
 export default function MemoForm() {
+  const [value, setValue] = useState('sdfsgsdf');
+  const [type, setType] = useState<MemoType>('BOOK_CONTENT');
+  const handleTypeChange = (newType: MemoType) => {
+    setType(newType);
+  };
+
   return (
     <>
       <PageTitle title="메모 등록 페이지" />
-      <div>memoForm</div>
+      <Boxcontainer>
+        <MemoTypeSelect type={type} onChange={handleTypeChange} />
+        {/* <ReactQuill theme="snow" value={value} onChange={setValue} />; */}
+        <ReactQuill value={value} readOnly />
+      </Boxcontainer>
     </>
   );
 }
+
+/**
+ * page
+ * type
+ * Authority
+ * text contents
+ */
