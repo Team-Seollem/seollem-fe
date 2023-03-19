a175fe540b12f7b2bc6e0f608040c4c62b779cf8
seul-dev <seul.dev@gmail.com>
2023-03-19 11:50:35 +0900

    ✨ feat: [#56] memoPageInput 컴포넌트

--- src/pages/MemoForm.tsx
+++ src/pages/MemoForm.tsx
@@ -5,17 +5,23 @@ import { Boxcontainer, PageTitle } from '@components/common';
 import { MemoAuthority, MemoType } from '@projects/types/library';
 import MemoTypeSelect from '@components/MemoForm/MemoTypeSelect';
 import MemoAuthoritySelect from '@components/MemoForm/MemoAuthoritySelect';
+import MemoPageInput from '@components/MemoForm/MemoPageInput';
 
 export default function MemoForm() {
-  const [value, setValue] = useState('sdfsgsdf');
-  const [type, setType] = useState<MemoType>('BOOK_CONTENT');
-  const handleTypeChange = (newType: MemoType) => {
-    setType(newType);
-  };
   const [authority, setAuthority] = useState<MemoAuthority>('PRIVATE');
-
+  const [page, setPage] = useState<number>(0);
+  const [type, setType] = useState<MemoType>('BOOK_CONTENT');
+  const [content, setContent] = useState('');
   const handleAuthorityChange = (newAuthority: MemoAuthority) => {
     setAuthority(newAuthority);
+  };
+
+  const handlePageChanage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = Math.max(0, Math.min(400, Number(e.target.value)));
+    setPage(value);
+  };
+  const handleTypeChange = (newType: MemoType) => {
+    setType(newType);
   };
 
   return (
@@ -26,15 +32,10 @@ export default function MemoForm() {
           authority={authority}
           onChange={handleAuthorityChange}
         />
+        <MemoPageInput page={page} onChange={handlePageChanage} />
         <MemoTypeSelect type={type} onChange={handleTypeChange} />
-        {/* <ReactQuill theme="snow" value={value} onChange={setValue} />; */}
-        <ReactQuill value={value} readOnly />
+        <ReactQuill theme="snow" value={content} onChange={setContent} />
       </Boxcontainer>
     </>
   );
 }
-
-/**
- * page
- * text contents
- */
