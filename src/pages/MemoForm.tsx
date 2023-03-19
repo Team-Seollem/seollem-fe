f63fb9521f7bb8848010913dc199e060ff86d9b9
seul-dev <seul.dev@gmail.com>
2023-03-19 19:23:33 +0900

    ✨ feat: [#56] text editor 기본 기능

--- src/pages/MemoForm.tsx
+++ src/pages/MemoForm.tsx
@@ -1,41 +1,67 @@
 import { useState } from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
 import { Boxcontainer, PageTitle } from '@components/common';
-import { MemoAuthority, MemoType } from '@projects/types/library';
+import type { MemoAuthority, MemoType } from '@projects/types/library';
 import MemoTypeSelect from '@components/MemoForm/MemoTypeSelect';
 import MemoAuthoritySelect from '@components/MemoForm/MemoAuthoritySelect';
 import MemoPageInput from '@components/MemoForm/MemoPageInput';
+import styled from 'styled-components';
+import MemoContentEditor from '@components/MemoForm/MemoContentEditor';
+import Button from '@components/common/Button';
 
 export default function MemoForm() {
-  const [authority, setAuthority] = useState<MemoAuthority>('PRIVATE');
-  const [page, setPage] = useState<number>(0);
-  const [type, setType] = useState<MemoType>('BOOK_CONTENT');
-  const [content, setContent] = useState('');
+  const [memoAuthority, setMemoAuthority] = useState<MemoAuthority>('PRIVATE');
+  const [memoPage, setMemoPage] = useState<number>(0);
+  const [memoType, setMemoType] = useState<MemoType>('BOOK_CONTENT');
+  const [memoContent, setMemoContent] = useState('');
   const handleAuthorityChange = (newAuthority: MemoAuthority) => {
-    setAuthority(newAuthority);
+    setMemoAuthority(newAuthority);
   };
-
   const handlePageChanage = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = Math.max(0, Math.min(400, Number(e.target.value)));
-    setPage(value);
+    setMemoPage(value);
   };
   const handleTypeChange = (newType: MemoType) => {
-    setType(newType);
+    setMemoType(newType);
+  };
+  const handleSumbit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log({
+      memoAuthority,
+      memoPage,
+      memoType,
+      memoContent,
+    });
   };
 
   return (
     <>
       <PageTitle title="메모 등록 페이지" />
       <Boxcontainer>
-        <MemoAuthoritySelect
-          authority={authority}
-          onChange={handleAuthorityChange}
-        />
-        <MemoPageInput page={page} onChange={handlePageChanage} />
-        <MemoTypeSelect type={type} onChange={handleTypeChange} />
-        <ReactQuill theme="snow" value={content} onChange={setContent} />
+        <SForm onSubmit={handleSumbit}>
+          <MemoAuthoritySelect
+            authority={memoAuthority}
+            onChange={handleAuthorityChange}
+          />
+          <MemoPageInput page={memoPage} onChange={handlePageChanage} />
+          <MemoTypeSelect type={memoType} onChange={handleTypeChange} />
+          <MemoContentEditor content={memoContent} onChange={setMemoContent} />
+          <SButton
+            type="submit"
+            styleType={!memoContent ? 'outlineDisabled' : 'solidPositive'}
+            size="large"
+          >
+            저장하기
+          </SButton>
+        </SForm>
       </Boxcontainer>
     </>
   );
 }
+const SForm = styled.form`
+  width: 100%;
+`;
+
+const SButton = styled(Button)`
+  font-size: ${({ theme }) => theme.fontSize.base};
+  margin-top: 1rem;
+`;
