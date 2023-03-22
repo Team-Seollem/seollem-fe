923664ca32b5cc8e9bfb1f7caeef664d2a0730cf
StarryPro <hansunguk6178@gmail.com>
2023-03-22 23:32:53 +0900

    :sparkles: feat: postBook Page 및 component 구현

--- src/pages/PostBookPage.tsx
+++ src/pages/PostBookPage.tsx
@@ -1,8 +1,15 @@
 import PostBook from '@components/PostBook';
+import { SearchBookInfo } from '@projects/types/basic';
 import { useLocation } from 'react-router-dom';
 
+type LocationState = {
+  state: {
+    bookInfoData: SearchBookInfo;
+  };
+};
+
 function PostBookPage() {
-  const location = useLocation();
+  const location = useLocation() as LocationState;
   const { bookInfoData } = location.state;
   return <PostBook bookInfoData={bookInfoData} />;
 }
