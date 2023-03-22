857a9a8972515b6459a403ccb67520d7f18952d8
StarryPro <hansunguk6178@gmail.com>
2023-03-22 23:32:11 +0900

    :bug: fix: DetailBookInfo Page 및 component 수정

--- src/pages/DetailBookInfoPage.tsx
+++ src/pages/DetailBookInfoPage.tsx
@@ -1,8 +1,14 @@
 import DetailBookInfo from '@components/DetailBookInfo';
+import { SearchBookInfo } from '@projects/types/basic';
 import { useLocation } from 'react-router-dom';
 
+type LocationState = {
+  state: {
+    bookInfoData: SearchBookInfo;
+  };
+};
 function DetailBookInfoPage() {
-  const location = useLocation();
+  const location = useLocation() as LocationState;
   const { bookInfoData } = location.state;
   return <DetailBookInfo bookInfoData={bookInfoData} />;
 }
