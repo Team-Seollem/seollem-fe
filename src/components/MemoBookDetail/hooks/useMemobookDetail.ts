bc6b3037dbe238ab4a1e7c829838a06e99a6a137
seul-dev <seul.dev@gmail.com>
2023-03-24 08:12:59 +0900

    ✨ feat: [#59] 캐시키 수정, useMemobookDetail 구현

---
+++ src/components/MemoBookDetail/hooks/useMemobookDetail.ts
@@ -0,0 +1,33 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { memoService } from '@apis/index';
+import { CACHE_KEYS } from '@constants';
+import type { MemoBookDetail, MemoBookType } from '@projects/types/library';
+
+type Props = {
+  bookId: number;
+  page: number;
+  size: number;
+  memoType: MemoBookType;
+};
+
+const fallback: MemoBookDetail[] = [];
+
+export default function useMemobookDetail({ bookId, memoType }: Props) {
+  const { data, isLoading, hasNextPage, fetchNextPage } = useInfiniteQuery(
+    CACHE_KEYS.memoBooksDetail(bookId, memoType),
+    ({ pageParam = 1 }) =>
+      memoService.getMemoBooksByBookId(bookId, pageParam, 10, memoType),
+    {
+      getNextPageParam: (lastpage) => {
+        const { pageInfo } = lastpage;
+        const { totalPages, page: currentPage } = pageInfo;
+
+        return currentPage >= totalPages ? undefined : currentPage + 1;
+      },
+      staleTime: 1000 * 60 * 5,
+    }
+  );
+  const memoBooks = data?.pages.flatMap((page) => page.item) ?? fallback;
+
+  return { memoBooks, isLoading, hasNextPage, fetchNextPage };
+}
--- src/constants/cacheKey.ts
+++ src/constants/cacheKey.ts
@@ -1,14 +1,14 @@
-import { BookStatus } from '@projects/types/library';
+import { BookStatus, MemoBookType } from '@projects/types/library';
 
 export const CACHE_KEYS = {
   library: (bookStatus: BookStatus) => ['library', bookStatus],
   bookDetail: (bookId: number) => ['bookDetail', bookId],
   memoBooks: ['memobooks'],
   memoBooksPage: (page: number) => [...CACHE_KEYS.memoBooks, page],
-  memoBooksDetail: (bookId: number, page: number) => [
+  memoBooksDetail: (bookId: number, memoType: MemoBookType) => [
     ...CACHE_KEYS.memoBooks,
     'detail',
     bookId,
-    page,
+    memoType,
   ],
 };
