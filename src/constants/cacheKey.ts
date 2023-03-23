e51df35ebeefd551ce53ade472050bea5281fa49
seul-dev <seul.dev@gmail.com>
2023-03-23 19:40:41 +0900

    ✨ feat: [#59] cache key 수정

--- src/constants/cacheKey.ts
+++ src/constants/cacheKey.ts
@@ -3,9 +3,10 @@ import { BookStatus } from '@projects/types/library';
 export const CACHE_KEYS = {
   library: (bookStatus: BookStatus) => ['library', bookStatus],
   bookDetail: (bookId: number) => ['bookDetail', bookId],
-  memoBooks: (page: number) => ['memobooks', page],
+  memoBooks: ['memobooks'],
+  memoBooksPage: (page: number) => [...CACHE_KEYS.memoBooks, page],
   memoBooksDetail: (bookId: number, page: number) => [
-    'memobooks',
+    ...CACHE_KEYS.memoBooks,
     'detail',
     bookId,
     page,
