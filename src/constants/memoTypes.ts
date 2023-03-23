f41f0dd61e7de8c3b1e3098fbecdc4c58b3230cb
seul-dev <seul.dev@gmail.com>
2023-03-23 20:56:43 +0900

    ✨ feat: [#59] MemoBookType 추가

--- src/constants/memoTypes.ts
+++ src/constants/memoTypes.ts
@@ -6,3 +6,10 @@ export const MEMO_TYPES = {
 } as const;
 
 export const memoTypeList = Object.values(MEMO_TYPES);
+
+export const MEMO_BOOK_TYPES = {
+  ...MEMO_TYPES,
+  ALL: { typeValue: 'ALL', typeText: '전체 보기' },
+} as const;
+
+export const memoBookTypeList = Object.values(MEMO_BOOK_TYPES);
