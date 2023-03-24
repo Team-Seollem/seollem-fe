9dd41337863a6e720bffae6b0e7c82d2d8731134
seul-dev <seul.dev@gmail.com>
2023-03-25 08:50:41 +0900

    💄 design: [#59] styling 수정

--- src/components/MemoBookDetail/MemoBookPage.tsx
+++ src/components/MemoBookDetail/MemoBookPage.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/no-danger */
+import DOMPurify from 'dompurify';
+import styled from 'styled-components';
 import type { MemoBookDetail } from '@projects/types/library';
 import { getLatestUpdateDate } from '@utils';
 import { BgType, MEMO_BACKGROUND } from 'constants/memoBackground';
-import DOMPurify from 'dompurify';
-import styled from 'styled-components';
 
 type Props = {
   memo: MemoBookDetail;
@@ -12,6 +12,7 @@ type Props = {
 
 export default function MemoBookPage({ memo, memoBookBg }: Props) {
   const date = getLatestUpdateDate(memo.createdAt, memo.updatedAt);
+
   return (
     <Wrapper
       fontColor={MEMO_BACKGROUND[memoBookBg].color}
@@ -39,7 +40,6 @@ const Wrapper = styled.div<{ fontColor: string; imageUrl: string }>`
   height: 30rem;
   z-index: 0;
   background-image: url(${({ imageUrl }) => imageUrl});
-  font-size: ${({ theme }) => theme.fontSize.base};
   background-size: cover;
   color: ${({ fontColor }) => fontColor};
   font-family: 'RIDIBatang';
@@ -49,8 +49,15 @@ const MemoInfo = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 2rem;
+  font-size: ${({ theme }) => theme.fontSize.sm};
+  @media (min-width: 450px) {
+    font-size: ${({ theme }) => theme.fontSize.base};
+  }
 `;
 
 const MemoContent = styled.div`
-  font-size: ${({ theme }) => theme.fontSize.md};
+  font-size: ${({ theme }) => theme.fontSize.base};
+  @media (min-width: 450px) {
+    font-size: ${({ theme }) => theme.fontSize.md};
+  }
 `;
