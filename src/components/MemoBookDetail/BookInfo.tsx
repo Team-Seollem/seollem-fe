3622ed54fe750e87715428c643aaeafc8b8efd58
seul-dev <seul.dev@gmail.com>
2023-03-24 15:06:24 +0900

    ✨ feat: [#59] BookInfo component 구현(title, memodetal page 이동)

---
+++ src/components/MemoBookDetail/BookInfo.tsx
@@ -0,0 +1,47 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import useBookDetail from '@components/LibraryBook/hooks/useBookDetail';
+import { parseTitle } from 'utils/parseTitle';
+import { Button } from '@components/common';
+
+export default function BookInfo() {
+  const navigate = useNavigate();
+  const { bookId } = useParams();
+  const { title } = useBookDetail({ bookId: Number(bookId) });
+  const { title: mainTitle, subTitle } = parseTitle(title);
+
+  const handleMemoEdit = () => {
+    navigate(`/book/library/${bookId}`);
+  };
+  return (
+    <Wrapper>
+      <div>
+        <Title>{mainTitle}</Title>
+        {subTitle && <SubTitle>{subTitle}</SubTitle>}
+      </div>
+      <Button styleType="solidPositive" size="small" onClick={handleMemoEdit}>
+        메모 작성하러 가기
+      </Button>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 5fr 1fr;
+  align-items: flex-start;
+  gap: 1rem;
+  padding-bottom: 1rem;
+  border-bottom: 1px solid ${({ theme }) => theme.color.gray03};
+  margin-bottom: 1rem;
+`;
+const Title = styled.h2`
+  font-size: ${({ theme }) => theme.fontSize.md};
+  color: ${({ theme }) => theme.color.black};
+`;
+
+const SubTitle = styled.h3`
+  font-size: ${({ theme }) => theme.fontSize.base};
+  color: ${({ theme }) => theme.color.gray01};
+  margin-top: 0.5rem;
+`;
