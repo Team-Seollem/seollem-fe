a87738f5a3d04f39107b4650e9219c386a32b889
seul-dev <seul.dev@gmail.com>
2023-03-25 08:08:07 +0900

    ✨ feat: [#59] memoBg image path 수정

--- src/constants/memoBackground.ts
+++ src/constants/memoBackground.ts
@@ -10,7 +10,7 @@ export const MEMO_BACKGROUND = {
     value: 'AURORA',
     btnImageUrl: new URL('../assets/memoBgBtn/btn-aurora.png', import.meta.url)
       .href,
-    imageUrl: new URL('../assets/memoBg/aurora.png', import.meta.url),
+    imageUrl: new URL('../assets/memoBg/aurora.png', import.meta.url).href,
     color: '#f9f9f9',
   },
   BLUE: {
@@ -19,7 +19,8 @@ export const MEMO_BACKGROUND = {
       '../assets/memoBgBtn/btn-blugradient.png',
       import.meta.url
     ).href,
-    imageUrl: new URL('../assets/memoBg/blugradient.png', import.meta.url).href,
+    imageUrl: new URL('../assets/memoBg/bluegradient.png', import.meta.url)
+      .href,
     color: '#f9f9f9',
   },
   PINK: {
