362d6fc0d7cc7d004b858e3356b16ca401dca2de
seul-dev <seul.dev@gmail.com>
2023-03-24 22:09:57 +0900

    ✨ feat: [#59] memobookviewer state관리 훅  구현

--- src/pages/MemoBookDetail.tsx
+++ src/pages/MemoBookDetail.tsx
@@ -1,11 +1,14 @@
 import { PageTitle } from '@components/common';
 import { PAGE_URL } from '@constants';
+import MemoBookViewer from '@components/MemoBookDetail/MemoBookViewer';
+import BookInfo from '@components/MemoBookDetail/BookInfo';
 
 export default function MemoBookDetail() {
   return (
     <>
       <PageTitle title="나만의 작은책 보기" path={PAGE_URL.MEMO_BOOKS} />
-      <div>memobook</div>
+      <BookInfo />
+      <MemoBookViewer />
     </>
   );
 }
