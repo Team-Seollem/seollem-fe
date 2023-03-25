7a409c9de580c33b565f62b0a3040083c35544c0
seul-dev <seul.dev@gmail.com>
2023-03-25 10:45:15 +0900

    ✨ feat: [#59] memo download 기능 추가

--- src/components/MemoBookDetail/MemoBookPage.tsx
+++ src/components/MemoBookDetail/MemoBookPage.tsx
@@ -1,6 +1,10 @@
 /* eslint-disable react/no-danger */
+import { useRef } from 'react';
 import DOMPurify from 'dompurify';
+import { toPng } from 'html-to-image';
 import styled from 'styled-components';
+import { FiDownload } from 'react-icons/fi';
+
 import type { MemoBookDetail } from '@projects/types/library';
 import { getLatestUpdateDate } from '@utils';
 import { BgType, MEMO_BACKGROUND } from 'constants/memoBackground';
@@ -13,25 +17,45 @@ type Props = {
 export default function MemoBookPage({ memo, memoBookBg }: Props) {
   const date = getLatestUpdateDate(memo.createdAt, memo.updatedAt);
 
+  const imgRef = useRef<HTMLDivElement>(null);
+
+  const downloadImage = async () => {
+    if (!imgRef.current) return;
+    const dataUrl = await toPng(imgRef.current);
+
+    const link = document.createElement('a');
+    link.download = 'selloem-memo.png';
+    link.href = dataUrl;
+    link.click();
+  };
+
   return (
-    <Wrapper
-      fontColor={MEMO_BACKGROUND[memoBookBg].color}
-      imageUrl={MEMO_BACKGROUND[memoBookBg].imageUrl}
-    >
-      <MemoInfo>
-        <p>{memo.memoBookPage}</p>
-        <p>{date}</p>
-      </MemoInfo>
-      <MemoContent>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: DOMPurify.sanitize(memo.memoContent),
-          }}
-        />
-      </MemoContent>
-    </Wrapper>
+    <DownloadWrapper>
+      <Wrapper
+        fontColor={MEMO_BACKGROUND[memoBookBg].color}
+        imageUrl={MEMO_BACKGROUND[memoBookBg].imageUrl}
+        ref={imgRef}
+      >
+        <MemoInfo>
+          <p>{memo.memoBookPage}</p>
+          <p>{date}</p>
+        </MemoInfo>
+        <MemoContent>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(memo.memoContent),
+            }}
+          />
+        </MemoContent>
+      </Wrapper>
+      <DownloadIcon onClick={downloadImage} />
+    </DownloadWrapper>
   );
 }
+
+const DownloadWrapper = styled.div`
+  position: relative;
+`;
 
 const Wrapper = styled.div<{ fontColor: string; imageUrl: string }>`
   display: flex;
@@ -60,4 +84,13 @@ const MemoContent = styled.div`
   @media (min-width: 450px) {
     font-size: ${({ theme }) => theme.fontSize.md};
   }
+`;
+
+const DownloadIcon = styled(FiDownload)`
+  font-size: 2.5rem;
+  color: white;
+  cursor: pointer;
+  position: absolute;
+  right: 20px;
+  bottom: 20px;
 `;
