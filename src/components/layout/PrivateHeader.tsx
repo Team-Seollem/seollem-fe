0ea923d148d3a7c6eb608a08a2ae7963345ec2c4
seul-dev <seul.dev@gmail.com>
2023-03-27 16:18:06 +0900

    ✨ feat: [#61] header 컴포넌트 수정

--- src/components/layout/PrivateHeader.tsx
+++ src/components/layout/PrivateHeader.tsx
@@ -1,17 +1,23 @@
 import { useNavigate } from 'react-router-dom';
 import { useSetRecoilState } from 'recoil';
 
-import { IoNotifications, IoPerson, IoLogOutOutline } from 'react-icons/io5';
+import { IoNotifications, IoMenu } from 'react-icons/io5';
 
 import { PAGE_URL } from '@constants';
 import { authService } from '@apis';
 import { loginState } from '@state/atom';
+import Dropdown from '@components/common/DropDown';
+import styled from 'styled-components';
 import * as S from './styles';
 import Logo from './Logo';
 
 export default function PrivateHeader() {
   const setIsLoggedIn = useSetRecoilState(loginState);
   const navigate = useNavigate();
+
+  const handleMyPage = () => {
+    navigate(PAGE_URL.MYPAGE);
+  };
 
   const handleLogout = () => {
     authService.signOut();
@@ -24,11 +30,16 @@ export default function PrivateHeader() {
       <Logo to={PAGE_URL.LIBRARY} />
 
       <S.Menu>
-        <IoNotifications />
-        <S.MenuLink to={PAGE_URL.MYPAGE}>
-          <IoPerson />
-        </S.MenuLink>
-        <IoLogOutOutline onClick={handleLogout} />
+        <Dropdown target={<IoMenu />}>
+          <S.DropdownMenuWrapper>
+            <S.DropdownMenuItem onClick={handleMyPage}>
+              마이 페이지
+            </S.DropdownMenuItem>
+            <S.DropdownMenuItem onClick={handleLogout}>
+              로그아웃
+            </S.DropdownMenuItem>
+          </S.DropdownMenuWrapper>
+        </Dropdown>
       </S.Menu>
     </S.Header>
   );
