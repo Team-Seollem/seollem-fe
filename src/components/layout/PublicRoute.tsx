b5740ab1ad77eebe4b208900879c6ed0e634803c
seul-dev <seul.dev@gmail.com>
2023-03-28 20:33:40 +0900

    🐛 fix: [#68] PrivateRoute, PublicRoute 수정

--- src/components/layout/PublicRoute.tsx
+++ src/components/layout/PublicRoute.tsx
@@ -1,21 +1,22 @@
-import { Outlet, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 import useLoginState from '@hooks/useLoginState';
 import { PAGE_URL } from '@constants';
 import { PublicHeader } from '@components/layout';
 import * as S from '@components/layout/styles';
 
-function PublicRoute() {
-  const { isLoggedIn } = useLoginState();
+type Props = {
+  children: React.ReactNode;
+};
 
+function PublicRoute({ children }: Props) {
+  const { isLoggedIn } = useLoginState();
   if (isLoggedIn) return <Navigate to={PAGE_URL.LIBRARY} />;
 
   return (
     <>
       <PublicHeader />
-      <S.Main>
-        <Outlet />
-      </S.Main>
+      <S.Main>{children}</S.Main>
     </>
   );
 }
