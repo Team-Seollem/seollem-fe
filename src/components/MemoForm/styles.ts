f0dcac2a496395d1ac5904564df0d7ab3b225899
seul-dev <seul.dev@gmail.com>
2023-04-10 19:35:53 +0900

    🐛 fix: [#74] editor 마진 추가 - 모바일에서 에디터에 마진이 없어서 버튼이 가려지는 문제 해결

--- src/components/MemoForm/styles.ts
+++ src/components/MemoForm/styles.ts
@@ -3,9 +3,10 @@ import styled from 'styled-components';
 
 export const Form = styled.form`
   width: 100%;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const FormButton = styled(Button)`
   font-size: ${({ theme }) => theme.fontSize.base};
-  margin-top: 1rem;
 `;
