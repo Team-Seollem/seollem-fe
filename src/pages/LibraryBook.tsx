84b18bff083834534034e83b7164ca64e74894ee
StarryPro <hansunguk6178@gmail.com>
2023-04-19 11:42:26 +0900

    :sparkles: feat: BookDetail component 구현

--- src/pages/LibraryBook.tsx
+++ src/pages/LibraryBook.tsx
@@ -1,4 +1,5 @@
 import { PageTitle } from '@components/common';
+import BookDetail from '@components/LibraryBook/BookDetail';
 import MemoList from '@components/LibraryBook/MemoList';
 import { PAGE_URL } from '@constants';
 
@@ -6,6 +7,7 @@ export default function LibraryBook() {
   return (
     <>
       <PageTitle title="책 상세 페이지" path={PAGE_URL.LIBRARY} />
+      <BookDetail />
       <MemoList />
     </>
   );
