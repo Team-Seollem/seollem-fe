0c8ee88da48565227280e7af8624bfdb6d3ac677
seul-dev <seul.dev@gmail.com>
2023-04-26 23:31:39 +0900

    ✨ feat: [#97] MemberItem에 suffix 추가

--- src/components/Community/Members.tsx
+++ src/components/Community/Members.tsx
@@ -3,12 +3,20 @@ import styled from 'styled-components';
 import useMembers from './hook/useMembers';
 import MemberItem from './MemberItem';
 
-const memberListType = {
-  mostReadMember: '가장 책을 많이 읽은 회원 📚',
-  mostMemoedMember: '가장 많은 메모를 남긴 회원 📝',
+export const memberListType = {
+  mostReadMember: {
+    label: '가장 책을 많이 읽은 회원 📚',
+    suffix: '권',
+  },
+  mostMemoedMember: {
+    label: '가장 많은 메모를 남긴 회원 📝',
+    suffix: '개',
+  },
 };
 
 type MemberListType = keyof typeof memberListType;
+export type SuffixType =
+  typeof memberListType[keyof typeof memberListType]['suffix'];
 
 type Props = {
   type: MemberListType;
@@ -17,12 +25,14 @@ type Props = {
 export default function Members({ type }: Props) {
   const { data: membersList } = useMembers();
 
+  const { label, suffix } = memberListType[type];
+
   return (
     <Section>
-      <Title>{memberListType[type]}</Title>
+      <Title>{label}</Title>
       <ListWrapper>
         {membersList[type].map((member) => (
-          <MemberItem key={member.memberId} member={member} />
+          <MemberItem key={member.memberId} member={member} suffix={suffix} />
         ))}
       </ListWrapper>
     </Section>
