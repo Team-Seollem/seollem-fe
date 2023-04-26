68bae1747f5f587e39c270c8e18e674df301614b
seul-dev <seul.dev@gmail.com>
2023-04-26 12:56:10 +0900

    ✨ feat: [#85] profile edit 기능 추가

--- src/components/Profile/MyProfile.tsx
+++ src/components/Profile/MyProfile.tsx
@@ -1,6 +1,8 @@
 import { Boxcontainer } from '@components/common';
 import useMyProfile from './hook/useMyProfile';
 import ProfileImageUpload from './ProfileImageUpload';
+import EditField from './EditField';
+import Field from './Field';
 
 export default function MyProfile() {
   const { data, isLoading } = useMyProfile();
@@ -8,6 +10,9 @@ export default function MyProfile() {
   return (
     <Boxcontainer>
       {!isLoading && <ProfileImageUpload src={data.url} />}
+      <EditField field="name" value={data.name} />
+      <Field field="email" fieldText={data.email} />
+      <EditField field="content" value={data.content} />
     </Boxcontainer>
   );
 }
