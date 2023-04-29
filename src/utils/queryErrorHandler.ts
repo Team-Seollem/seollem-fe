40aee377d13b32d4b838255a60bbf5c7d0efe3da
seul-dev <seul.dev@gmail.com>
2023-04-30 02:39:07 +0900

    ✨ feat: [#97] queryErrorHandler에 api error message fallback 추가

--- src/utils/queryErrorHandler.ts
+++ src/utils/queryErrorHandler.ts
@@ -4,7 +4,10 @@ import { toast } from 'react-toastify';
 export const queryErrorHandler = (error: unknown) => {
   let message = 'Error connecting to server';
   if (error instanceof AxiosError && error.response) {
-    message = error.response.data.message || '잠시 후 다시 시도해보세요';
+    message =
+      error.response.data.message ||
+      error.response.data ||
+      '잠시 후 다시 시도해보세요';
   }
   toast.error(message);
 };
