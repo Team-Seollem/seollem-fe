25d6d5bfb58e992dbb0902c146d53b7145817619
seul-dev <seul.dev@gmail.com>
2023-04-30 20:59:39 +0900

    ♻️  refactor: [#97] BookTitle 컴포넌트 분리

--- src/components/MemoBookDetail/BookInfo.tsx
+++ src/components/MemoBookDetail/BookInfo.tsx
@@ -1,14 +1,13 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import useBookDetail from '@components/LibraryBook/hooks/useBookDetail';
-import { parseTitle } from 'utils/parseTitle';
 import { Button } from '@components/common';
+import BookTitle from '@components/common/BookTitle';
 
 export default function BookInfo() {
   const navigate = useNavigate();
   const { bookId } = useParams();
   const { title } = useBookDetail({ bookId: Number(bookId) });
-  const { title: mainTitle, subTitle } = parseTitle(title);
 
   const handleMemoEdit = () => {
     navigate(`/book/library/${bookId}`);
@@ -16,8 +15,7 @@ export default function BookInfo() {
   return (
     <Wrapper>
       <div>
-        <Title>{mainTitle}</Title>
-        {subTitle && <SubTitle>{subTitle}</SubTitle>}
+        <BookTitle title={title} />
       </div>
       <Button styleType="solidPositive" size="small" onClick={handleMemoEdit}>
         메모 작성하러 가기
@@ -34,15 +32,4 @@ const Wrapper = styled.div`
   padding-bottom: 1rem;
   border-bottom: 1px solid ${({ theme }) => theme.color.gray03};
   margin-bottom: 1rem;
-`;
-const Title = styled.h2`
-  font-size: ${({ theme }) => theme.fontSize.md};
-  color: ${({ theme }) => theme.color.black};
-`;
-
-const SubTitle = styled.h3`
-  font-size: ${({ theme }) => theme.fontSize.base};
-  color: ${({ theme }) => theme.color.gray01};
-  margin-top: 0.5rem;
-  line-height: 1.5rem;
 `;
