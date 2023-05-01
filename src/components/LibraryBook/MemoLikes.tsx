956e130342a641e693bc2242e46c14bad655608f
seul-dev <seul.dev@gmail.com>
2023-05-01 14:31:29 +0900

    ✨ feat: [#97] MemoLikes 컴포넌트 기능 추가

--- src/components/LibraryBook/MemoLikes.tsx
+++ src/components/LibraryBook/MemoLikes.tsx
@@ -1,13 +1,61 @@
-import Badge from '@components/common/Badge';
+import styled from 'styled-components';
 import { FcLikePlaceholder, FcLike } from 'react-icons/fc';
+import Badge from '@components/common/Badge';
+import type { LikeMemoFn, LikeMyMemoFn } from './hooks/useMemoLike';
 
-type Props = {
+type BaisicProps = {
   likesCount: number;
+  memoLikeDone: boolean;
+  isMymemo: boolean;
 };
-export default function MemoLikes({ likesCount }: Props) {
+
+type MyMemoLikeProps = {
+  isMymemo: true;
+  likeMemo: LikeMyMemoFn;
+  unlikeMemo: LikeMyMemoFn;
+  memoId: number;
+} & BaisicProps;
+
+type MemberMemoLikeProps = {
+  isMymemo: false;
+  likeMemo: LikeMemoFn;
+  unlikeMemo: LikeMemoFn;
+  memoId: number;
+  memberId: number;
+  bookId: number;
+} & BaisicProps;
+
+type Props = MyMemoLikeProps | MemberMemoLikeProps;
+
+export default function MemoLikes(props: Props) {
+  const { likesCount, memoLikeDone, isMymemo, memoId, likeMemo, unlikeMemo } =
+    props;
+  const handleMemoLike = () => {
+    if (isMymemo) {
+      likeMemo(memoId);
+    } else {
+      const { memberId, bookId } = props;
+      likeMemo(memoId, memberId, bookId);
+    }
+  };
+
+  const handleMemoUnlike = () => {
+    if (isMymemo) {
+      unlikeMemo(memoId);
+    } else {
+      const { memberId, bookId } = props;
+      unlikeMemo(memoId, memberId, bookId);
+    }
+  };
+
   return (
-    <Badge>
-      {likesCount ? <FcLike /> : <FcLikePlaceholder />} {likesCount}
-    </Badge>
+    <Wrapper onClick={memoLikeDone ? handleMemoUnlike : handleMemoLike}>
+      <Badge>
+        {memoLikeDone ? <FcLike /> : <FcLikePlaceholder />} {likesCount}
+      </Badge>
+    </Wrapper>
   );
 }
+const Wrapper = styled.div`
+  cursor: pointer;
+`;
