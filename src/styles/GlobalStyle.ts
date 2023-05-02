894cd8f53b11e76b608d3cd35d6c578a6e3a2e20
seul-dev <seul.dev@gmail.com>
2023-05-03 03:32:29 +0900

    ✨ feat: [#100] landing page 추가

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -24,6 +24,13 @@ export const GlobalStyle = createGlobalStyle`
     font-style: normal;
   }
 
+  @font-face {
+    font-family: 'LINESeedKR-Bd';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/LINESeedKR-Bd.woff2') format('woff2');
+    font-weight: 700;
+    font-style: normal;
+  }
+
 
   *{
     box-sizing: border-box;
