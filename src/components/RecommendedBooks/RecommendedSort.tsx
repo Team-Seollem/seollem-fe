8fda0d302cf4ff89817694f78633345badcad5cb
seul-dev <seul.dev@gmail.com>
2023-05-22 19:57:31 +0900

    ✨ feat: [#106] 선택된 tab ui 추가

--- src/components/RecommendedBooks/RecommendedSort.tsx
+++ src/components/RecommendedBooks/RecommendedSort.tsx
@@ -3,20 +3,24 @@ import { RecommendSort } from '@projects/types/basic';
 import styled from 'styled-components';
 
 type Props = {
+  selectedSort: RecommendSort;
   onChangeList: (value: RecommendSort) => void;
 };
 
-function RecommendedSort({ onChangeList }: Props) {
-  const sortDataList = RECOMMENDED_SORT.map((data) => {
-    return (
-      <li key={data.id}>
-        <button type="button" onClick={() => onChangeList(data.value)}>
+function RecommendedSort({ selectedSort, onChangeList }: Props) {
+  return (
+    <SSortList>
+      {RECOMMENDED_SORT.map((data) => (
+        <STag
+          key={data.id}
+          isSelected={selectedSort === data.value}
+          onClick={() => onChangeList(data.value)}
+        >
           {data.name}
-        </button>
-      </li>
-    );
-  });
-  return <SSortList>{sortDataList}</SSortList>;
+        </STag>
+      ))}
+    </SSortList>
+  );
 }
 
 export default RecommendedSort;
@@ -27,20 +31,18 @@ const SSortList = styled.ul`
   gap: 1rem;
   margin-bottom: 1rem;
   border-bottom: solid 3px ${({ theme }) => theme.color.gray04};
+`;
+const STag = styled.li<{ isSelected: boolean }>`
+  background-color: transparent;
+  font-family: 'Pretendard-Regular';
+  cursor: pointer;
+  padding: 1rem;
+  color: ${({ theme, isSelected }) =>
+    isSelected ? theme.color.gray01 : theme.color.gray02};
+  border-bottom: ${({ theme, isSelected }) =>
+    isSelected ? `solid 1px ${theme.color.gray01}` : 'none'};
 
-  li:hover {
-    border-bottom: solid 1px ${({ theme }) => theme.color.gray01};
-  }
-
-  button {
-    background-color: transparent;
-    font-family: 'Pretendard-Regular';
-    cursor: pointer;
-    padding: 1rem;
-    color: ${({ theme }) => theme.color.gray02};
-    border: none;
-    &:hover {
-      color: ${({ theme }) => theme.color.gray01};
-    }
+  &:hover {
+    color: ${({ theme }) => theme.color.gray01};
   }
 `;
